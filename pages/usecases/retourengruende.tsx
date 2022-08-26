6d2c8784cf1f61303a338554901fd7c73940e59f
Matthias Nannt <mail@matthiasnannt.com>
2022-08-26 09:56:37 +0200

    update

--- pages/usecases/retourengruende.tsx
+++ pages/usecases/retourengruende.tsx
@@ -137,8 +137,7 @@ export default function BeachchairsPage({}) {
                   Die Lösung
                 </span>
                 <span className="block mt-2 text-3xl font-bold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl">
-                  Automatisierte Klassifizierung von Produkt-Bewertungen und
-                  Retourengründen
+                  Automatisierte Klassifizierung
                 </span>
               </h2>
             </div>
