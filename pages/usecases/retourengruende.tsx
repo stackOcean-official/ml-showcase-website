cacc77e81809d24e5d497408c7d1e51d999d7d5f
Matthias Nannt <mail@matthiasnannt.com>
2022-09-01 10:20:19 +0200

    fix retourengründe text

--- pages/usecases/retourengruende.tsx
+++ pages/usecases/retourengruende.tsx
@@ -269,7 +269,7 @@ export default function BeachchairsPage({}) {
                   Ähnliche Anwendungsfälle
                 </span>
                 <span className="block mt-2 text-3xl font-bold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl">
-                  Stimmungsanalyse mit KI
+                  Automatisierte Text-Klassifizierung
                 </span>
               </h2>
             </div>
